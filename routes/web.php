69bcfeb048f14978d7e86aa12bfa8a0a7dd4be15
許睿舜 <steven921107@yahoo.com.tw>
2025-01-03 20:38:36 +0800

    使用 create方法

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-    $post = new Post();
-    $post->title = "test title";
-    $post->content = "test content";
-    $post->save();
+    //return view('welcome');
+    //$post = new Post();
+    //$post->title = "test title";
+    //$post->content = "test content";
+    //$post->save();
+
+    Post::create([
+        'title' => 'created title',
+        'content' => 'created content',
+    ]);
 });
